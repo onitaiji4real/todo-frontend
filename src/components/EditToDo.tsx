d6bc5fd9b450cd0525605d6bfd28c4db51071aa1
Hank_Chen <hank_chen@rosati.com.tw>
2024-07-15 20:45:57 +0800

    feat: add editToDo

---
+++ src/components/EditToDo.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Task } from "./ToDoTable";
+import { PrioritySelect } from "./PrioritySelect";
+import { DatePicker } from "./DatePicker";
+import { Checkbox } from "./ui/checkbox";
+import { format } from "date-fns";
+
+export function EditToDo({ task }: { task: Task }) {
+  const [editedTask, setEditedTask] = React.useState<Task>({
+    ...task,
+    Deadline: new Date(task.Deadline),
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setEditedTask((prevTask) => ({ ...prevTask, [id]: value }));
+  };
+
+  const handlePriorityChange = (priority: string) => {
+    setEditedTask((prevTask) => ({ ...prevTask, Priority: priority }));
+  };
+
+  const handleDateChange = (date: Date) => {
+    // 移除时间部分，只保留日期部分
+    const newDate = new Date(
+      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+    );
+    setEditedTask((prevTask) => ({
+      ...prevTask,
+      Deadline: newDate,
+    }));
+  };
+
+  const handleStatusChange = (checked: boolean) => {
+    setEditedTask((prevTask) => ({
+      ...prevTask,
+      isDone: checked ? 1 : 0,
+    }));
+  };
+
+  const handleSave = async () => {
+    const { _id, ...updatedTaskWithoutId } = {
+      ...editedTask,
+      Deadline: editedTask.Deadline.toISOString().split("T")[0],
+    };
+
+    console.log(updatedTaskWithoutId);
+
+    try {
+      const response = await fetch(`http://localhost:8787/todos/${_id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTaskWithoutId),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update task");
+      }
+
+      const result = await response.json();
+      console.log("Update successful:", result);
+    } catch (error) {
+      console.error("Error updating task:", error);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit task</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="Task" className="text-right">
+              Task
+            </Label>
+            <Input
+              id="Task"
+              value={editedTask.Task}
+              onChange={handleInputChange}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="Priority" className="text-right">
+              Priority
+            </Label>
+            <PrioritySelect
+              priority={editedTask.Priority}
+              onChange={handlePriorityChange}
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="Deadline" className="text-right">
+              Deadline
+            </Label>
+            <DatePicker
+              currentDate={editedTask.Deadline}
+              onDateChange={handleDateChange}
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="isDone" className="text-right">
+              Status
+            </Label>
+            <div className="flex items-center space-x-2">
+              <Checkbox
+                checked={editedTask.isDone === 1}
+                onCheckedChange={handleStatusChange}
+              />
+              <label
+                htmlFor="isDone"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Done
+              </label>
+            </div>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={handleSave}>
+            Save
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ src/components/ToDoTable.tsx
@@ -0,0 +1,297 @@
+"use client";
+
+import * as React from "react";
+import useSWR from "swr";
+import axios from "axios";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "./ui/badge";
+import { fetcher } from "@/lib/fetcher";
+import { EditToDo } from "./EditToDo";
+
+export type Task = {
+  _id: string;
+  Task: string;
+  Priority: string;
+  Deadline: Date;
+  isDone: number;
+};
+
+export const columns: ColumnDef<Task>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "Task",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Task
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("Task")}</div>,
+  },
+  {
+    accessorKey: "Priority",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Priority
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <Badge
+        className="capitalize"
+        variant={
+          row.getValue("Priority") === "High"
+            ? "destructive"
+            : row.getValue("Priority") === "Medium"
+            ? ""
+            : "secondary"
+        }
+      >
+        {row.getValue("Priority")}
+      </Badge>
+    ),
+  },
+  {
+    accessorKey: "Deadline",
+    header: () => <div className="text-right">Deadline</div>,
+    cell: ({ row }) => {
+      const deadline = new Date(row.getValue("Deadline"));
+      return (
+        <div className="text-right font-medium">
+          {deadline.toLocaleDateString("zh-TW", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          })}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "isDone",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="capitalize">
+        {row.getValue("isDone") === 1 ? "Done" : "UnDone"}
+      </div>
+    ),
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const task = row.original;
+
+      return <EditToDo task={task} />;
+    },
+  },
+];
+
+export function ToDoTable() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const url = "http://localhost:8787/todos"; // 正確的 API URL
+
+  const { data, isLoading, error } = useSWR<Task[]>(url, fetcher);
+
+  const table = useReactTable({
+    data: data || [], // 修正 data 類型錯誤
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data</div>;
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter tasks..."
+          value={(table.getColumn("Task")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("Task")?.setFilterValue(event.target.value)
+          }
+          className="max-w-64"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
